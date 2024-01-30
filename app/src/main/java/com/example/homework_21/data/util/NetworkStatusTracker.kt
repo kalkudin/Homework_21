f918c346fb1ee89e1b2b325913e2a4fea1030065
nikoloz <nikolozkaladze@gmail.com>
2024-01-30 10:56:58 -0800

    updated some features

--- app/src/main/java/com/example/homework_21/data/util/NetworkStatusTracker.kt
+++ app/src/main/java/com/example/homework_21/data/util/NetworkStatusTracker.kt
@@ -5,20 +5,21 @@ import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkRequest
-import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
-class NetworkStatusTracker @Inject constructor (context: Context) : NetworkChecker {
+class NetworkStatusTracker @Inject constructor(context: Context) : NetworkChecker {
+
     private val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-    private val _networkStatus = MutableLiveData<Boolean>()
+    private val _networkStatus = MutableStateFlow<Boolean>(isConnected())
 
     private val networkCallback = object : ConnectivityManager.NetworkCallback() {
         override fun onAvailable(network: Network) {
-            _networkStatus.postValue(true)
+            _networkStatus.value = true
         }
 
         override fun onLost(network: Network) {
-            _networkStatus.postValue(false)
+            _networkStatus.value = false
         }
     }
 
